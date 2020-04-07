8716fb64b880742612edb49539a0fa0a8ea2872e
小葵 <xi4okui@gmail.com>
2020-04-07 14:31:02 +0800

    Update main.py

--- 0x01/main.py
+++ 0x01/main.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def ctf():
+    return "<script>location='error'</script> Flag{302 is good!}"
+
+@app.route("/error/")
+def error():
+    return "Error!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
